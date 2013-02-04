d3343f4ebd53b43dbebd525f7c024f40af9c41e1
Raymond T. Hightower <rth@wisdomgroup.com>
2013-02-03 20:01:44 -0600

    colors_controller is set, now turning attention to app_delegate

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -1,2 +1,21 @@
 # Let's do this with ctags!
+class ColorsController < UIViewController
+
+  def viewDidLoad
+
+    super
+
+    self.view.backgroundColor = UIcolor.whiteColor
+
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "Colors"
+    @label.sizeToFit
+    @label.center = [self.view.frame.size.width / 2, self.view.frame.size.height / 2]
+    @label.autoresizingMask = (UIViweAutoresizingFlexibleBottommargin | UIViewAutoresizingFlexibleTopMargin)
+
+    self.view.addSubview(@label)    
+
+
+  end
+end
 
