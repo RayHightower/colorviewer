ff9cfd270e8d3b85f65e0c542e16354542af8474
Raymond T. Hightower <rth@wisdomgroup.com>
2013-02-04 16:29:41 -0600

    experimenting with a random controller

---
+++ app/controllers/random_controller.rb
@@ -0,0 +1,13 @@
+# random_controller.rb was added to test order of compilation by RubyMotion.
+# This controller is not designed to do anything special.
+# Why are you still reading this?
+#
+
+class RandomController < UIViewController
+
+  def init
+    super
+    
+  end
+
+end
